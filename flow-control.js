d58c68571a238d806c862f0bd136951a27db5143
sinduja <sindhu.38@gmail.com>
2019-05-01 04:02:43 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
   else {
     return " You are not a teenager"
   }
-  
+
   }
 
 
@@ -41,22 +41,22 @@ function switchAge(age) {
   switch(age)
   {
     case 13:
-   return 'You are a teenager!' 
+   return 'You are a teenager!'
       case 14:
-      return 'You are a teenager!' 
+      return 'You are a teenager!'
       case 15:
-      return 'You are a teenager!' 
+      return 'You are a teenager!'
     case 16:
-    return 'You are a teenager!' 
+    return 'You are a teenager!'
     case 17:
     return 'You are a teenager!' 
 
  case 18:
-return 'You are a teenager!' 
+return 'You are a teenager!'
     case 19:
-    return 'You are a teenager!' 
+    return 'You are a teenager!'
     default:
     return 'You have an age'
-    
+
   }
 }
